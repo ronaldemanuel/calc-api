9aeebc48e8dd8369c043d4f2172084c3023ed861
ronaldemanuel <emronald15el@gmail.com>
2021-11-03 10:26:06 -0300

    alterando formato da resposta

--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,7 @@ const app = express();
 app.get('/soma/:num1/:num2', (req, res) => {
     const num1 = parseFloat(req.params.num1);
     const num2 = parseFloat(req.params.num2);
-    res.send(num1 + ' + ' + num2 + ' = ' + `${num1 + num2}`);
+    res.send({resposta: num1+num2});
 });
 
 module.exports = app;
